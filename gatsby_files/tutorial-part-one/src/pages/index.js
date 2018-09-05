d2998b3f1f794ef958a4655d545f7fcf4742ea7e
Nichole Libby <xpyrrh@gmail.com>
2018-09-05 16:34:20 -0600

    updated index.js in accordance with tut

--- gatsby_files/tutorial-part-one/src/pages/index.js
+++ gatsby_files/tutorial-part-one/src/pages/index.js
@@ -1,3 +1,13 @@
-import React from "react"
+import React from "react";
+import Link from "gatsby-link";
 
-export default () => <div>Hello world!</div>
+export default () => <div style={{color: 'tomato'}}>
+  <h1>Hello Gatsby!</h1>
+  <p>this is quite a world</p>
+  <img src = "https://source.unsplash.com/random/400x200" alt="random photo" />
+  <br />
+  <br />
+  <div>
+    <Link to="/page-2/">Link</Link>
+  </div>
+</div>
