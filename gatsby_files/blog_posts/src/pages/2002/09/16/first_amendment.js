bd03d0f003b1e7a06542ee0ceb3526d31998b31c
Nichole Libby <xpyrrh@gmail.com>
2018-09-06 19:02:01 -0600

    .

---
+++ gatsby_files/blog_posts/src/pages/2002/09/16/first_amendment.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+
+  <p>
+    Posted: 2002-09-16 12:55:57 <br /><br />
+
+    <a href="http://www.freedomforum.org/templates/document.asp?documentID=16836\\">
+a sad day:</a>
+    <q> For the first time in the annual State of the First Amendment survey, almost
+  half (49%) of those surveyed said the First Amendment goes too far in the rights
+ it guarantees.
+    </q>
+ </p>
+);
