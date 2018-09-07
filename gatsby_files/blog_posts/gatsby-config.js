bd1fe791151dfee24d696c4fcbdaac509d955682
Nichole Libby <xpyrrh@gmail.com>
2018-09-06 19:01:50 -0600

    temporary (?) file structure change to allow URL preservation

--- gatsby_files/blog_posts/gatsby-config.js
+++ gatsby_files/blog_posts/gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [{
+    resolve: 'gatsby-plugin-canonical-urls',
+    options: {
+      siteUrl: 'https://www.typewriting.org',
+    }
+  },
+  'gatsby-plugin-react-helmet'],
 }
