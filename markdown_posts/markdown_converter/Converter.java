052fa661099a04c6c698c8b5868cb010df80c8b8
Nichole Libby <xpyrrh@gmail.com>
2018-09-09 14:17:42 -0600

    trailing characters have been handled. needs refactoring due to code repeats

--- markdown_posts/markdown_converter/Converter.java
+++ markdown_posts/markdown_converter/Converter.java
@@ -16,7 +16,37 @@ public class Converter {
         String[] split = read.split(",'");
 
         for (int i = 0; i < split.length; ++i) {
+          int length = split[i].length();
+//remove beginning paren
+          if (split[i] != null && length > 0 && split[i].charAt(0) == '(') {
+            split[i] = split[i].substring(1, length);
+          }
+
+//remove ending semicolon
+          length = split[i].length();
+          if (split[i] != null && length > 0 &&
+                split[i].charAt(length-1) == ';') {
+            split[i] = split[i].substring(0, length-1);
+          }
+
+
+//remove ending paren
+          length = split[i].length();
+          if (split[i] != null && length > 0 &&
+                split[i].charAt(length-1) == ')') {
+            split[i] = split[i].substring(0, length-1);
+          }
+
+//remove trailing apostrophes
+          length = split[i].length();
+          if (split[i] != null && length > 0 &&
+                split[i].charAt(length-1) == '\'') {
+            split[i] = split[i].substring(0, length-1);
+          }
+
+
           System.out.println(split[i]);
+
         }
 
       }
