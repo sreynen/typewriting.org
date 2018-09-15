18353fe7e9d5266f5b71032f9e14752c6b507d1d
Nichole Libby <xpyrrh@gmail.com>
2018-09-14 18:48:29 -0600

    .

---
+++ gatsby_files/blog_posts/gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://www.typewriting.org',
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: '${__dirname}/md_files',
+        name: 'pages'
+      }
+    },
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark'
+  ],
+}
