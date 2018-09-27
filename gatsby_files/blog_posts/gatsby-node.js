f3677b8362d79783560301461441c7ab5934afc9
Nichole Libby <xpyrrh@gmail.com>
2018-09-26 20:01:27 -0600

    fixed the env; still code errors in gatsby-node.js

--- gatsby_files/blog_posts/gatsby-node.js
+++ gatsby_files/blog_posts/gatsby-node.js
@@ -11,43 +11,45 @@ const path = require('path');
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
 	const { createPage } = boundActionCreators;
-}
 
-const blogPostTemplate = path.resolve(`../../markdown_posts/md_files/templates/blog-post.js`);
 
-return graphql(`{
-	allMarkdownRemark (
-		sort: { order: DESC, fields: [frontmatter__date] }
-		limit: 1000
-	) {
-		edges {
-			node {
-				excerpt(pruneLength: 250)
-				html
-				id
-				frontmatter {
-					date
-					path
-					title
+	return new Promise((resolve,reject) => {
+		const blogPostTemplate = path.resolve(`../../markdown_posts/md_files/templates/blog-post.js`);
+		resolve(
+			graphql(`{
+				allMarkdownRemark (
+					sort: { order: DESC, fields: [frontmatter__date] }
+					limit: 1000
+				) {
+					edges {
+						node {
+							excerpt(pruneLength: 250)
+							html
+							id
+							frontmatter {
+								date
+								path
+								title
+							}
+						}
+					}
 				}
-			}
-		}
+			}`)
+				.then(result => {
+					if (result.errors) {
+						reject(result.errors);
+					}
+				}
+				result.data.allMarkdownRemark.edges
+					.forEach(({ node }) =>{
+						createPage({
+							path: node.frontmatter.path,
+							component: blogPostTemplate,
+							context: {} // additional data can be passed via context
+						});
+					});
+				});
+			)
+		})
 	}
-}`)
-	.then(result => {
-		if (result.errors) {
-			return Promise.reject(result.errors);
-		}
-	}
-	result.data.allMarkdownRemark.edges
-		.forEach(({ node }) =>{
-			createPage({
-				path: node.frontmatter.path,
-				component: blogPostTemplate,
-				context: {} // additional data can be passed via context
-			});
-		});
-	});
 }
-
-
