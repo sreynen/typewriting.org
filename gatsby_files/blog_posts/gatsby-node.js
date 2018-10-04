8f2501c52e5630661b4ef956fdb5ee8559b1016e
Nichole Libby <xpyrrh@gmail.com>
2018-10-04 15:54:18 -0600

    code changes due to build errors

--- gatsby_files/blog_posts/gatsby-node.js
+++ gatsby_files/blog_posts/gatsby-node.js
@@ -18,9 +18,10 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
 		resolve(
 			graphql(`{
 				allMarkdownRemark (
-					sort: { order: DESC, fields: [frontmatter__date] }
+					sort: { order: DESC, fields: [frontmatter___date] }
 					limit: 1000
-				) {
+				)
+				{
 					edges {
 						node {
 							excerpt(pruneLength: 250)
@@ -39,17 +40,15 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
 					if (result.errors) {
 						reject(result.errors);
 					}
-				}
-				result.data.allMarkdownRemark.edges
-					.forEach(({ node }) =>{
+
+					result.data.allMarkdownRemark.edges.forEach(({ node }) =>{
 						createPage({
 							path: node.frontmatter.path,
 							component: blogPostTemplate,
-							context: {} // additional data can be passed via context
-						});
-					});
-				});
+							context: {}, // additional data can be passed via context
+						})
+					})
+				})
 			)
 		})
 	}
-}
