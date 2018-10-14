14df196738f4f72688f6fb1ae73d1ffff2db4110
Nichole Libby <xpyrrh@gmail.com>
2018-10-13 19:40:49 -0600

    gatsby build failing due to data.markdownRemark being null/undefined. Need to debug

--- markdown_posts/md_files/templates/blog-post.js
+++ markdown_posts/md_files/templates/blog-post.js
@@ -1,23 +1,39 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { graphql } from "graphql";
 
 export default function Template({
-  data
+  data,
 }) {
-  const post = data.markdownRemark;
-  return(
-      <div className = "blog-post-container">
-        <Helmet title={'$post.frontmatter.title'} />
-          <div className = "blog-post">
-            <h1>{post.frontmatter.title}</h1>
-            <div
-              className = "blog-post-content"
-              dangerouslySetInnerHTML={{ __html: post.html }} //???
-            />
-          </div>
+  const { markdownRemark } = data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <div className="blog-post-container">
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html}}
+        />
       </div>
-  );
+    </div>
+  )
 }
+//   const post = data.markdownRemark;
+//   return(
+//       <div className = "blog-post-container">
+//         <Helmet title={'$post.frontmatter.title'} />
+//           <div className = "blog-post">
+//             <h1>{post.frontmatter.title}</h1>
+//             <div
+//               className = "blog-post-content"
+//               dangerouslySetInnerHTML={{ __html: post.html }} //???
+//             />
+//           </div>
+//       </div>
+//   );
+// }
 
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
