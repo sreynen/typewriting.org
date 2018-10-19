0c44fb17fcbefdc74f5b0661a2ddac63e0f8f7c7
Nichole Libby <xpyrrh@gmail.com>
2018-10-19 13:55:26 -0600

    develop & build working. Added several components to index.js; 1 warning for TemplateWrapper not used.

--- gatsby_files/fresh/src/pages/index.js
+++ gatsby_files/fresh/src/pages/index.js
@@ -1,11 +1,84 @@
 import React from 'react'
 import { Link } from 'gatsby'
 //import PropTypes from 'prop-types'
-//import Helmet from 'react-helmet'
+import Helmet from 'react-helmet'
+import Media from 'react-media'
 
 import Layout from '../components/layout'
 //import './index.css'
 import "../styles/layout-override.css"
+
+
+const TemplateWrapper = ({ children }) => (
+   <div>
+      <Helmet
+         title="Gatsby Default Starter"
+         meta={[
+            { name: "description", content: "Sample" },
+            { name: "keywords", content: "sample, something" }
+         ]}
+      />
+      <Header />
+      <div
+         style={{
+            margin: "0 auto",
+            maxWidth: 980,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            height: "100%"
+         }}
+      >
+      <Media query={{ maxWidth: 848 }}>
+         {matches =>
+            matches ? (
+               <div
+                  style={{
+                     margin: "0 auto",
+                     maxWidth: 980,
+                     display: "flex",
+                     flexDirection: "row",
+                     justifyContent: "space-between",
+                     height: "100%",
+                     padding: "25px"
+                  }}
+               >
+                  <div style={{ flex: 1 }}>{children()}</div>
+               </div>
+            ) : (
+               <div
+                  style={{
+                     margin: "0 auto",
+                     maxWidth: 980,
+                     display: "flex",
+                     flexDirection: "row",
+                     justifyContent: "space-between",
+                     height: "100%",
+                     padding: "25px"
+                  }}
+               >
+                  <div style={{ flex: 2.5, paddingRight: "30px" }}>
+                     {children()}
+                  </div>
+
+                  <div style={{ flex: 1 }}>
+                     <Sidebar
+                        title="test"
+                        description="Test of the early warning system."
+                     />
+                     <Sidebar
+                        title="About"
+                        description="This is not a real alarm"
+                     />
+                     </div>
+                  </div>
+               )
+            }
+         </Media>
+      </div>
+   </div>
+);
+
 
 const Header = () => (
    <div
@@ -33,6 +106,19 @@ const Header = () => (
             </Link>
          </h1>
       </div>
+   </div>
+);
+
+const Sidebar = (props) => (
+   <div
+      style={{
+         border: '2px solid #e6e6e6',
+         maxWidth: 960,
+         padding: '0.5rem',
+         marginBottom: '25px'
+      }}
+   >
+      <strong>{props.title}.</strong> {props.description}
    </div>
 );
 
