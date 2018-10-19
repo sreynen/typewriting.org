2517457e5e0a2dc5146ecdc27c6c56c910ab43f2
Nichole Libby <xpyrrh@gmail.com>
2018-10-19 13:37:17 -0600

    develop & build are working. compile warning d/t Header not being used in index.js

--- gatsby_files/fresh/src/pages/index.js
+++ gatsby_files/fresh/src/pages/index.js
@@ -4,9 +4,37 @@ import { Link } from 'gatsby'
 //import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
-//import '.index.css'
-//import "../styles/layout-override.css"
+//import './index.css'
+import "../styles/layout-override.css"
 
+const Header = () => (
+   <div
+      style={{
+         background:'#f5f5f5',
+         marginBottom: '3rem',
+         borderBottom: '2px solid #e6e6e6',
+      }}
+   >
+      <div
+         style={{
+            margin: '0 auto',
+            maxWidth: 980,
+            padding: '1.45rem 1.0875rem',
+         }}
+      >
+         <h1 style={{margin: 0, textAlign: 'center', fontSize: '18px'}}>
+            <Link to="/"
+               style={{
+                  color: 'black',
+                  textDecoration: 'none',
+               }}
+            >
+               CodeStack
+            </Link>
+         </h1>
+      </div>
+   </div>
+);
 
 const IndexPage = () => (
   <Layout>
