ce3b92a8b87224a52b72c5330aac118d9f9f96e0
Nichole Libby <xpyrrh@gmail.com>
2018-10-21 12:20:37 -0600

    .

--- markdown_posts/markdown_converter/Converter.java
+++ markdown_posts/markdown_converter/Converter.java
@@ -87,7 +87,7 @@ public class Converter {
       String path = "/" + replaceSubstring (split[1], " ", "_");
       //writing cleaned contents out to markdown goes here
 
-      FileWriter writeOut = new FileWriter("../../gatsby_files/blog_posts/md_files/" + split[1] + ".md");
+      FileWriter writeOut = new FileWriter("../../gatsby_files/typewriting_posts/md_files/" + split[1] + ".md");
       //create post header per tutorial
       writeOut.write("---" + "\n");
       writeOut.write("path: \"" +  dateTime.split(" ")[0] + path + "\" \n");
